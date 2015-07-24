d94e6528e4bfa95e618e1e2ebfa033e64ac0610d
sagi <sagi@tx.technion.ac.il>
2015-07-24 19:50:50 +0300

    Added read/write functions that checks for access permissions

--- libgpufs/fat_pointer.cu.h
+++ libgpufs/fat_pointer.cu.h
@@ -25,23 +25,6 @@
 __device__ volatile void* gmmap_warp(void *addr, size_t size, int prot, int flags, int fd, off_t offset);
 __device__ int gmunmap_warp( volatile void *addr, size_t length );
 
-static const unsigned int FID_BITS = 4;
-static const unsigned int VPAGE_BITS = 28;
-static const unsigned int REF_COUNT_BITS = 64 - FID_BITS - VPAGE_BITS - (32 - FS_LOGBLOCKSIZE);
-static const unsigned int FFU_BITS = 32 - 1 - VPAGE_BITS;
-
-static const unsigned int INVALID_VPAGE = (1 << VPAGE_BITS) - 1;
-static const unsigned int INVALID_FID = (1 << FID_BITS) - 1;
-
-struct __align__(8) _FatPtr
-{
-	unsigned int pageOffset : FS_LOGBLOCKSIZE;
-	unsigned int physicalPage : (32 - FS_LOGBLOCKSIZE);
-	unsigned int vpage : VPAGE_BITS;
-	unsigned int ffu : FFU_BITS;
-	unsigned int valid : 1;
-};
-
 __forceinline__ __device__ unsigned long long getTicks()
 {
 	unsigned long long ticks;
@@ -49,9 +32,49 @@ __forceinline__ __device__ unsigned long long getTicks()
 	return ticks;
 }
 
+__forceinline__ __device__ int acc2Bitfeild( int acc )
+{
+	switch (acc) {
+		case O_GRDONLY 	: return 0x1;
+		case O_GWRONLY 	: return 0x2;
+		case O_GCREAT 	: return 0x4;
+		case O_GRDWR 	: return 0x3;
+		case O_GWRONCE 	: return 0x6;
+		default 		: return 0;
+	}
+}
+
+__forceinline__ __device__ int bitfeild2Acc( int bits )
+{
+	switch (bits) {
+		case 0x1 	: return O_GRDONLY;
+		case 0x2 	: return O_GWRONLY;
+		case 0x4 	: return O_GCREAT;
+		case 0x3 	: return O_GRDWR;
+		case 0x6 	: return O_GWRONCE;
+		default 	: return 0;
+	}
+}
+
+static const unsigned int ACCESS_BITS = 3;
+static const unsigned int VPAGE_BITS = 64 - 32 - 1 - ACCESS_BITS; // 28
+
+static const unsigned int FID_BITS = 64 - (32 - FS_LOGBLOCKSIZE) - VPAGE_BITS;
+
+static const unsigned int INVALID_VPAGE = (1 << VPAGE_BITS) - 1;
+static const unsigned int INVALID_FID = (1 << FID_BITS) - 1;
+
+struct __align__(8) _FatPtr
+{
+	unsigned int pageOffset 	: FS_LOGBLOCKSIZE;
+	unsigned int physicalPage 	: (32 - FS_LOGBLOCKSIZE);
+	unsigned int vpage 			: VPAGE_BITS;
+	unsigned int accBits 		: ACCESS_BITS;
+	unsigned int valid 			: 1;
+};
+
 struct __align__(8) _TlbLine
 {
-	unsigned int refCount : REF_COUNT_BITS;
 	unsigned int physicalPage : (32 - FS_LOGBLOCKSIZE);
 	unsigned int vpage : VPAGE_BITS;
 	unsigned int fid : FID_BITS;
@@ -69,7 +92,6 @@ struct TLB
 		{
 			for( int i = 0; i < N; ++i )
 			{
-				lines[i].refCount = 0;
 				lines[i].physicalPage = 0;
 				lines[i].vpage = INVALID_VPAGE;
 				lines[i].fid = INVALID_FID;
@@ -98,12 +120,13 @@ class FatPointer
 {
 public:
 	__device__ FatPointer( size_t fid, off_t start, size_t size, int flags, TLB<N>* tlb, uchar* mem, volatile PFrame* frames ) :
-	m_fid(fid), m_start(start), m_end(start + size), m_flags(flags)
+	m_fid(fid), m_start(start), m_end(start + size)
 	{
-		m_ptr.vpage = start >> FS_LOGBLOCKSIZE;
-		m_ptr.valid = 0;
-		m_ptr.physicalPage = 0;
-		m_ptr.pageOffset = 0;
+		m_ptr.pageOffset 	= 0;
+		m_ptr.physicalPage 	= 0;
+		m_ptr.vpage 		= start >> FS_LOGBLOCKSIZE;
+		m_ptr.accBits 		= acc2Bitfeild( flags );
+		m_ptr.valid 		= 0;
 
 		m_tlb = tlb;
 
@@ -123,7 +146,7 @@ public:
 
 	__device__ FatPointer( const FatPointer& ptr ) :
 		m_fid(ptr.m_fid), m_start(ptr.m_start), m_end(ptr.m_end), m_ptr(ptr.m_ptr),
-		m_tlb(ptr.m_tlb), m_mem(ptr.m_mem), m_frames(ptr.m_frames), m_flags(ptr.m_flags)
+		m_tlb(ptr.m_tlb), m_mem(ptr.m_mem), m_frames(ptr.m_frames)
 	{
 		// Copies are invalid by definition
 		m_ptr.valid = 0;
@@ -144,7 +167,6 @@ public:
 		m_fid = ptr.m_fid;
 		m_start = ptr.m_start;
 		m_end = ptr.m_end;
-		m_flags = ptr.m_flags;
 
 		m_ptr.vpage = ptr.m_ptr.vpage;
 		m_ptr.physicalPage = ptr.m_ptr.physicalPage;
@@ -369,7 +391,7 @@ public:
 						}
 
 //						time2Start = getTicks();
-						volatile void* ptr = gmmap_warp(NULL, FS_BLOCKSIZE, 0, m_flags, m_fid, (size_t)query << FS_LOGBLOCKSIZE);
+						volatile void* ptr = gmmap_warp(NULL, FS_BLOCKSIZE, 0, bitfeild2Acc( m_ptr.accBits ), m_fid, (size_t)query << FS_LOGBLOCKSIZE);
 //						time2Stop = getTicks();
 //						count1++;
 //						time2 += time2Stop - time2Start;
@@ -417,6 +439,402 @@ public:
 		return *((T*)pRet);
 	}
 
+	__forceinline__ __device__ T read()
+	{
+//		time1Start = getTicks();
+		if( ( m_ptr.accBits & 0x1 ) == 0 )
+		{
+			GPU_ASSERT(false);
+			return 0;
+		}
+
+		long long* t = reinterpret_cast<long long*>(&m_ptr);
+
+		int valid = (*t < 0);
+		int allValid = __all( valid );
+
+		if( allValid )
+		{
+			int* ptr = reinterpret_cast<int*>(&m_ptr);
+			uchar* pRet = m_mem + *ptr;
+
+//			time1Stop = getTicks();
+//			time1 += time1Stop - time1Start;
+
+			return *((T*)pRet);
+		}
+
+		while( true )
+		{
+			valid = (*t < 0);
+			int invalidThread = __ballot( !valid );
+
+			int leader = __ffs( invalidThread );
+
+			if( 0 == leader )
+			{
+				// No invalid threads
+				break;
+			}
+
+			// Correction because __ffs start counting from 1;
+			leader -= 1;
+
+			int query = m_ptr.vpage;
+			query = __shfl( query, leader );
+
+			int want = (m_ptr.vpage == query);
+			int wantThreads = __ballot( want );
+			int numWants = __popc( wantThreads );
+
+			int physical = 0;
+
+			size_t h = hash( query );
+			volatile _TlbLine &line = m_tlb->lines[h];
+
+			int* pRefCount = &(m_tlb->locks[h]);
+
+			int old;
+
+			if( LANE_ID == 0 )
+			{
+				old = atomicAdd( pRefCount, numWants );
+				DBGT( "start", WARP_ID, old, threadIdx.x );
+			}
+			old = __shfl( old, 0 );
+
+			if( (old >= 0) && (line.fid = m_fid) && (line.vpage == query) )
+			{
+				// Found the page in the tlb
+				physical = line.physicalPage;
+			}
+			else
+			{
+				// TODO: Add open addressing around here
+
+				// Wrong page, decrease ref count
+				if( LANE_ID == 0 )
+				{
+					old = atomicSub( pRefCount, numWants );
+					DBGT( "revert", WARP_ID, old, threadIdx.x );
+				}
+
+				while( true )
+				{
+					if( LANE_ID == 0 )
+					{
+						old = atomicCAS(pRefCount, 0, INT_MIN);
+						DBGT( "cas", WARP_ID, old, threadIdx.x );
+					}
+					old = __shfl( old, 0 );
+
+//					DBGT( "o", WARP_ID, old, threadIdx.x );
+
+					if( old > 0 )
+					{
+						DBGT( "positive", WARP_ID, *pRefCount, threadIdx.x );
+						if( (line.fid == m_fid) && (line.vpage == query) )
+						{
+							// Someone added our line? maybe?
+							if( LANE_ID == 0 )
+							{
+								old = atomicAdd( pRefCount, numWants );
+								DBGT( "retry", WARP_ID, old, threadIdx.x );
+							}
+							old = __shfl( old, 0 );
+
+							// Let's double check
+							if( (old >= 0) && (line.fid == m_fid) && (line.vpage == query) )
+							{
+								// Found the page in the tlb
+								physical = line.physicalPage;
+								break;
+							}
+							else
+							{
+								// False alarm
+								if( LANE_ID == 0 )
+								{
+									old = atomicSub( pRefCount, numWants );
+									DBGT( "revert retry", WARP_ID, old, threadIdx.x );
+								}
+								old = __shfl( old, 0 );
+
+								continue;
+							}
+						}
+						else
+						{
+							DBGT( "positive-1", WARP_ID, *pRefCount, threadIdx.x );
+							// Not our page
+							continue;
+						}
+					}
+					else if( old < 0 )
+					{
+						// line is locked
+						continue;
+					}
+					else
+					{
+						// We locked the page, now we can do whatever we want
+						// First check if we are evicting an existing map
+						if( line.fid != INVALID_FID )
+						{
+							if( LANE_ID == 0 )
+							{
+								m_frames[line.physicalPage].unlock_rw();
+							}
+						}
+
+//						time2Start = getTicks();
+						volatile void* ptr = gmmap_warp(NULL, FS_BLOCKSIZE, 0, bitfeild2Acc( m_ptr.accBits ), m_fid, (size_t)query << FS_LOGBLOCKSIZE);
+//						time2Stop = getTicks();
+//						count1++;
+//						time2 += time2Stop - time2Start;
+
+						if( LANE_ID == 0 )
+						{
+							physical = ((size_t)ptr - (size_t)m_mem) >> FS_LOGBLOCKSIZE;
+
+
+							line.fid = m_fid;
+							line.vpage = query;
+							line.physicalPage = physical;
+						}
+
+						threadfence();
+
+						if( LANE_ID == 0 )
+						{
+							old = atomicAdd(pRefCount, numWants - INT_MIN);
+							DBGT( "unlock", WARP_ID, old, threadIdx.x );
+//							GDBG( "numWants", WARP_ID, numWants );
+//							GDBG( "pRefCount", WARP_ID, *pRefCount );
+						}
+
+						break;
+					}
+				}
+			}
+
+			physical = __shfl( physical, 0 );
+
+			if( want )
+			{
+				m_ptr.physicalPage = physical;
+				m_ptr.valid = 1;
+			}
+		}
+
+		int* ptr = reinterpret_cast<int*>(&m_ptr);
+		uchar* pRet = m_mem + *ptr;
+
+//		time1Stop = getTicks();
+//		time1 += time1Stop - time1Start;
+
+		return *((T*)pRet);
+	}
+
+	__forceinline__ __device__ void write(T in)
+	{
+//		time1Start = getTicks();
+		if( ( m_ptr.accBits & 0x2 ) == 0 )
+		{
+			GPU_ASSERT(false);
+			return;
+		}
+
+		long long* t = reinterpret_cast<long long*>(&m_ptr);
+
+		int valid = (*t < 0);
+		int allValid = __all( valid );
+
+		if( allValid )
+		{
+			int* ptr = reinterpret_cast<int*>(&m_ptr);
+			uchar* pRet = m_mem + *ptr;
+
+//			time1Stop = getTicks();
+//			time1 += time1Stop - time1Start;
+
+			*((T*)pRet) = in;
+			return;
+		}
+
+		while( true )
+		{
+			valid = (*t < 0);
+			int invalidThread = __ballot( !valid );
+
+			int leader = __ffs( invalidThread );
+
+			if( 0 == leader )
+			{
+				// No invalid threads
+				break;
+			}
+
+			// Correction because __ffs start counting from 1;
+			leader -= 1;
+
+			int query = m_ptr.vpage;
+			query = __shfl( query, leader );
+
+			int want = (m_ptr.vpage == query);
+			int wantThreads = __ballot( want );
+			int numWants = __popc( wantThreads );
+
+			int physical = 0;
+
+			size_t h = hash( query );
+			volatile _TlbLine &line = m_tlb->lines[h];
+
+			int* pRefCount = &(m_tlb->locks[h]);
+
+			int old;
+
+			if( LANE_ID == 0 )
+			{
+				old = atomicAdd( pRefCount, numWants );
+				DBGT( "start", WARP_ID, old, threadIdx.x );
+			}
+			old = __shfl( old, 0 );
+
+			if( (old >= 0) && (line.fid = m_fid) && (line.vpage == query) )
+			{
+				// Found the page in the tlb
+				physical = line.physicalPage;
+			}
+			else
+			{
+				// TODO: Add open addressing around here
+
+				// Wrong page, decrease ref count
+				if( LANE_ID == 0 )
+				{
+					old = atomicSub( pRefCount, numWants );
+					DBGT( "revert", WARP_ID, old, threadIdx.x );
+				}
+
+				while( true )
+				{
+					if( LANE_ID == 0 )
+					{
+						old = atomicCAS(pRefCount, 0, INT_MIN);
+						DBGT( "cas", WARP_ID, old, threadIdx.x );
+					}
+					old = __shfl( old, 0 );
+
+//					DBGT( "o", WARP_ID, old, threadIdx.x );
+
+					if( old > 0 )
+					{
+						DBGT( "positive", WARP_ID, *pRefCount, threadIdx.x );
+						if( (line.fid == m_fid) && (line.vpage == query) )
+						{
+							// Someone added our line? maybe?
+							if( LANE_ID == 0 )
+							{
+								old = atomicAdd( pRefCount, numWants );
+								DBGT( "retry", WARP_ID, old, threadIdx.x );
+							}
+							old = __shfl( old, 0 );
+
+							// Let's double check
+							if( (old >= 0) && (line.fid == m_fid) && (line.vpage == query) )
+							{
+								// Found the page in the tlb
+								physical = line.physicalPage;
+								break;
+							}
+							else
+							{
+								// False alarm
+								if( LANE_ID == 0 )
+								{
+									old = atomicSub( pRefCount, numWants );
+									DBGT( "revert retry", WARP_ID, old, threadIdx.x );
+								}
+								old = __shfl( old, 0 );
+
+								continue;
+							}
+						}
+						else
+						{
+							DBGT( "positive-1", WARP_ID, *pRefCount, threadIdx.x );
+							// Not our page
+							continue;
+						}
+					}
+					else if( old < 0 )
+					{
+						// line is locked
+						continue;
+					}
+					else
+					{
+						// We locked the page, now we can do whatever we want
+						// First check if we are evicting an existing map
+						if( line.fid != INVALID_FID )
+						{
+							if( LANE_ID == 0 )
+							{
+								m_frames[line.physicalPage].unlock_rw();
+							}
+						}
+
+//						time2Start = getTicks();
+						volatile void* ptr = gmmap_warp(NULL, FS_BLOCKSIZE, 0, bitfeild2Acc( m_ptr.accBits ), m_fid, (size_t)query << FS_LOGBLOCKSIZE);
+//						time2Stop = getTicks();
+//						count1++;
+//						time2 += time2Stop - time2Start;
+
+						if( LANE_ID == 0 )
+						{
+							physical = ((size_t)ptr - (size_t)m_mem) >> FS_LOGBLOCKSIZE;
+
+
+							line.fid = m_fid;
+							line.vpage = query;
+							line.physicalPage = physical;
+						}
+
+						threadfence();
+
+						if( LANE_ID == 0 )
+						{
+							old = atomicAdd(pRefCount, numWants - INT_MIN);
+							DBGT( "unlock", WARP_ID, old, threadIdx.x );
+//							GDBG( "numWants", WARP_ID, numWants );
+//							GDBG( "pRefCount", WARP_ID, *pRefCount );
+						}
+
+						break;
+					}
+				}
+			}
+
+			physical = __shfl( physical, 0 );
+
+			if( want )
+			{
+				m_ptr.physicalPage = physical;
+				m_ptr.valid = 1;
+			}
+		}
+
+		int* ptr = reinterpret_cast<int*>(&m_ptr);
+		uchar* pRet = m_mem + *ptr;
+
+//		time1Stop = getTicks();
+//		time1 += time1Stop - time1Start;
+
+		*((T*)pRet) = in;
+	}
+
+
 	__device__ FatPointer& operator += ( size_t offset )
 	{
 		return move(offset);
@@ -444,7 +862,6 @@ public:
 	size_t m_fid;	// Should be spilled to local memory
 	off_t m_start;	// Should be spilled to local memory
 	size_t m_end;	// Should be spilled to local memory
-	int m_flags;	// Should be spilled to local memory
 
 	_FatPtr m_ptr;
 
