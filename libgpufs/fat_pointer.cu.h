0b161dc361030ee8fa711b8aa51ccbe162f769e8
sagi <sagi@tx.technion.ac.il>
2015-08-03 10:48:51 +0300

    combined TLB and noTLB implementations

--- libgpufs/fat_pointer.cu.h
+++ libgpufs/fat_pointer.cu.h
@@ -19,6 +19,7 @@
 #define FAT_POINTER_CU
 
 #include <limits.h>
+#include <type_traits>
 #include "fs_constants.h"
 #include "util.cu.h"
 
@@ -118,12 +119,12 @@ struct __align__(16) _TlbLine
 };
 
 template<int N>
-struct TLB
+struct _nonEmptyTLB
 {
 	_TlbLine lines[N];
 	int locks[N];
 
-	__device__ TLB()
+	__device__ _nonEmptyTLB()
 	{
 		if( LANE_ID == 0 )
 		{
@@ -138,7 +139,7 @@ struct TLB
 		}
 	}
 
-	__device__ ~TLB()
+	__device__ ~_nonEmptyTLB()
 	{
 		if( LANE_ID == 0 )
 		{
@@ -153,11 +154,99 @@ struct TLB
 	}
 };
 
-template<typename T, int N>
-class FatPointer
+template<int N>
+struct _emptyTLB
 {
+};
+
+template<int N>
+struct TLB : public std::conditional<N == 0, _emptyTLB<N>, _nonEmptyTLB<N>>::type
+{
+//	_TlbLine lines[N];
+//	int locks[N];
+//
+//	__device__ TLB()
+//	{
+//		if( LANE_ID == 0 )
+//		{
+//			for( int i = WARP_ID; i < N; i += NUM_WARPS )
+//			{
+//				DBGT( "i", WARP_ID, i, threadIdx.x );
+//				lines[i].physPage = 0;
+//				lines[i].virtPage = INVALID_VPAGE;
+//				lines[i].fid = INVALID_FID;
+//				locks[i] = 0;
+//			}
+//		}
+//	}
+//
+//	__device__ ~TLB()
+//	{
+//		if( LANE_ID == 0 )
+//		{
+//			for( int i = WARP_ID; i < N; i += NUM_WARPS )
+//			{
+//				if( lines[i].fid != INVALID_FID )
+//				{
+//					g_ppool->frames[lines[i].physPage].unlock_rw();
+//				}
+//			}
+//		}
+//	}
+};
+
+template<typename T, int N> class _FatPointerWithTLB;
+template<typename T, int N> class _FatPointerNoTLB;
+
+template<typename T, int N>
+class FatPointer : public std::conditional<N == 0, _FatPointerNoTLB<T,N>, _FatPointerWithTLB<T,N>>::type
+{
+	typedef typename std::conditional<N == 0, _FatPointerNoTLB<T,N>, _FatPointerWithTLB<T,N>>::type Base;
+
 public:
 	__device__ FatPointer( size_t fid, off_t start, size_t size, int flags, TLB<N>* tlb, uchar* mem, volatile PFrame* frames ) :
+		Base( fid, start, size, flags, tlb, mem, frames)
+		{
+		}
+
+	__device__ FatPointer& operator=( const FatPointer& ptr )
+	{
+		return Base::operator=(ptr);
+	}
+
+	__device__ FatPointer& moveTo( size_t offset )
+	{
+		Base::moveTo(offset);
+		return *this;
+	}
+
+	__forceinline__ __device__ FatPointer& move( size_t offset )
+	{
+		Base::move(offset);
+		return *this;
+	}
+
+	__forceinline__ __device__ T& operator *()
+	{
+		return Base::operator*();
+	}
+
+	__device__ FatPointer& operator += ( size_t offset )
+	{
+		return move(offset);
+	}
+
+	__device__ FatPointer& operator -= ( size_t offset )
+	{
+		return move(-offset);
+	}
+};
+
+template<typename T, int N>
+class _FatPointerWithTLB
+{
+public:
+	__device__ _FatPointerWithTLB( size_t fid, off_t start, size_t size, int flags, TLB<N>* tlb, uchar* mem, volatile PFrame* frames ) :
 	m_fid(fid), m_start(start), m_end(start + size)
 	{
 		m_ptr.virtPage = start >> FS_LOGBLOCKSIZE;
@@ -171,7 +260,7 @@ public:
 		m_frames = (PFrame*)frames;
 	}
 
-	__device__ FatPointer( const FatPointer& ptr ) :
+	__device__ _FatPointerWithTLB( const _FatPointerWithTLB& ptr ) :
 	m_fid(ptr.m_fid), m_start(ptr.m_start), m_end(ptr.m_end), m_ptr(ptr.m_ptr),
 	m_tlb(ptr.m_tlb), m_mem(ptr.m_mem), m_frames(ptr.m_frames)
 	{
@@ -181,7 +270,7 @@ public:
 		// TODO: update virtual address in case of physical pointer
 	}
 
-	__device__ ~FatPointer()
+	__device__ ~_FatPointerWithTLB()
 	{
 		bool resolved = false;
 
@@ -227,7 +316,7 @@ public:
 		}
 	}
 
-	__device__ FatPointer& operator=( const FatPointer& ptr )
+	__device__ _FatPointerWithTLB& operator=( const FatPointer<T,N>& ptr )
 	{
 		m_fid = ptr.m_fid;
 		m_start = ptr.m_start;
@@ -246,7 +335,7 @@ public:
 	}
 
 	// Move to exact offset from the beginning of the map
-	__device__ FatPointer& moveTo( size_t offset )
+	__device__ _FatPointerWithTLB& moveTo( size_t offset )
 	{
 		if( m_ptr.valid )
 		{
@@ -300,7 +389,7 @@ public:
 	}
 
 	// Move to offset from current location
-	__forceinline__ __device__ FatPointer& move( size_t offset )
+	__forceinline__ __device__ _FatPointerWithTLB& move( size_t offset )
 	{
 		offset *= sizeof(T);
 
@@ -560,12 +649,12 @@ public:
 		return *((T*)pRet);
 	}
 
-	__device__ FatPointer& operator += ( size_t offset )
+	__device__ _FatPointerWithTLB& operator += ( size_t offset )
 	{
 		return move(offset);
  	}
 
-	__device__ FatPointer& operator -= ( size_t offset )
+	__device__ _FatPointerWithTLB& operator -= ( size_t offset )
 	{
 		return move(-offset);
 	}
@@ -610,6 +699,272 @@ public:
 //	unsigned long long time4;
 //	unsigned long long time4Start;
 //	unsigned long long time4Stop;
+};
+
+template<typename T, int N>
+class _FatPointerNoTLB
+{
+public:
+	__device__ _FatPointerNoTLB( size_t fid, off_t start, size_t size, int flags, TLB<N>* tlb, uchar* mem, volatile PFrame* frames ) :
+	m_fid(fid), m_start(start), m_end(start + size)
+	{
+		m_ptr.virtPage = start >> FS_LOGBLOCKSIZE;
+		m_ptr.valid = 0;
+		m_ptr.pageOffset = 0;
+		m_ptr.accBits = acc2Bitfeild( flags );
+
+		m_mem = mem;
+		m_frames = (PFrame*)frames;
+	}
+
+	__device__ _FatPointerNoTLB( const _FatPointerNoTLB<T,N>& ptr ) :
+	m_fid(ptr.m_fid), m_start(ptr.m_start), m_end(ptr.m_end),
+	m_ptr(ptr.m_ptr), m_mem(ptr.m_mem), m_frames(ptr.m_frames)
+	{
+		// Copies are invalid by definition
+		m_ptr.valid = 0;
+
+		// TODO: update virtual address in case of physical pointer
+	}
+
+	__device__ ~_FatPointerNoTLB()
+	{
+		bool resolved = false;
+
+		if( !m_ptr.valid )
+		{
+			resolved = true;
+		}
+
+		while( !resolved )
+		{
+			// Gather every thread that needs the same page
+			int invalidThreads = __ballot( 1 );
+
+			int leader = __ffs( invalidThreads );
+
+			// Correction because __ffs start counting from 1;
+			leader -= 1;
+
+			BroadcastHelper bHelper;
+			bHelper.l = m_ptr.physPage;
+			bHelper = broadcast( bHelper, leader );
+
+			int want = (m_ptr.physPage == bHelper.l);
+			int wantThreads = __ballot( want );
+			int numWants = __popc( wantThreads );
+
+			if( LANE_ID == leader )
+			{
+				m_frames[m_ptr.physPage].unlock_rw(numWants);
+			}
+
+			if( want )
+			{
+				resolved = true;
+			}
+		}
+	}
+
+	__device__ _FatPointerNoTLB& operator=( const FatPointer<T,N>& ptr )
+	{
+		m_fid = ptr.m_fid;
+		m_start = ptr.m_start;
+		m_end = ptr.m_end;
+
+		m_ptr = ptr.m_ptr;
+
+		// Copies are invalid by definition
+		m_ptr.valid = 0;
+
+		m_mem = ptr.m_mem;
+		m_frames = ptr.m_frames;
+
+		return *this;
+	}
+
+	// Move to exact offset from the beginning of the map
+	__device__ _FatPointerNoTLB& moveTo( size_t offset )
+	{
+		if( m_ptr.valid )
+		{
+			m_ptr.valid = 0;
+
+			bool resolved = false;
+
+			while( !resolved )
+			{
+				// Gather every thread that needs the same page
+				int invalidThreads = __ballot( 1 );
+
+				int leader = __ffs( invalidThreads );
+
+				// Correction because __ffs start counting from 1;
+				leader -= 1;
+
+				BroadcastHelper bHelper;
+				bHelper.l = m_ptr.physPage;
+				bHelper = broadcast( bHelper, leader );
+
+				int want = (m_ptr.physPage == bHelper.l);
+				int wantThreads = __ballot( want );
+				int numWants = __popc( wantThreads );
+
+				if( LANE_ID == leader )
+				{
+					m_frames[m_ptr.physPage].unlock_rw(numWants);
+				}
+
+				if( want )
+				{
+					resolved = true;
+				}
+			}
+
+			// Fall through
+		}
+
+		m_ptr.virtPage = (m_start + offset) >> FS_LOGBLOCKSIZE;
+		m_ptr.pageOffset = (m_start + offset) & FS_BLOCKMASK;
+
+		return *this;
+	}
+
+	// Move to offset from current location
+	__device__ _FatPointerNoTLB& move( size_t offset )
+	{
+		offset *= sizeof(T);
+
+		if( m_ptr.valid )
+		{
+			// Try to keep it valid
+			if( 0 == ((m_ptr.pageOffset + offset) >> FS_LOGBLOCKSIZE) )
+			{
+				// We're still in the same block, just update the physical offset
+				m_ptr.pageOffset += offset;
+				return *this;
+			}
+
+			// else
+			m_ptr.valid = 0;
+
+			bool resolved = false;
+
+			while( !resolved )
+			{
+				// Gather every thread that needs the same page
+				int invalidThreads = __ballot( 1 );
+
+				int leader = __ffs( invalidThreads );
+
+				// Correction because __ffs start counting from 1;
+				leader -= 1;
+
+				BroadcastHelper bHelper;
+				bHelper.l = m_ptr.physPage;
+				bHelper = broadcast( bHelper, leader );
+
+				int want = (m_ptr.physPage == bHelper.l);
+				int wantThreads = __ballot( want );
+				int numWants = __popc( wantThreads );
+
+				long long virtPage;
+
+				if( LANE_ID == leader )
+				{
+					virtPage = m_frames[m_ptr.physPage].file_offset >> FS_LOGBLOCKSIZE;
+					m_frames[m_ptr.physPage].unlock_rw(numWants);
+				}
+
+				bHelper.l = virtPage;
+				bHelper = broadcast( bHelper, leader );
+
+				if( want )
+				{
+					m_ptr.virtPage = bHelper.l;
+					resolved = true;
+				}
+			}
+			// Fall through
+		}
+
+		m_ptr.virtPage = m_ptr.virtPage + ((m_ptr.pageOffset + offset) >> FS_LOGBLOCKSIZE);
+		m_ptr.pageOffset = (m_ptr.pageOffset + offset) & FS_BLOCKMASK;
+
+		return *this;
+	}
+
+	__forceinline__ __device__ T& operator *()
+	{
+		int valid = (m_ptr.valid);
+		int allValid = __all( valid );
+
+		if( allValid )
+		{
+			uchar* pRet = m_mem + m_ptr.offset;
+			return *((T*)pRet);
+		}
+
+		while( true )
+		{
+			valid = (m_ptr.valid);
+			int invalidThread = __ballot( !valid );
+
+			int leader = __ffs( invalidThread );
+
+			if( 0 == leader )
+			{
+				// No invalid threads
+				break;
+			}
+
+			// Correction because __ffs start counting from 1;
+			leader -= 1;
+
+			BroadcastHelper bHelper;
+			bHelper.l = m_ptr.virtPage;
+			bHelper = broadcast( bHelper, leader );
+
+			int want = (m_ptr.virtPage == bHelper.l);
+			int wantThreads = __ballot( want );
+			int numWants = __popc( wantThreads );
+
+			volatile void* ptr = gmmap_warp(NULL, FS_BLOCKSIZE, 0, bitfeild2Acc( m_ptr.accBits ), m_fid, bHelper.l << FS_LOGBLOCKSIZE, numWants);
+			int physical = ((size_t)ptr - (size_t)m_mem) >> FS_LOGBLOCKSIZE;
+
+			physical = __shfl( physical, 0 );
+
+			if( want )
+			{
+				m_ptr.physPage = physical;
+				m_ptr.valid = 1;
+			}
+		}
+
+		uchar* pRet = m_mem + m_ptr.offset;
+		return *((T*)pRet);
+	}
+
+	__device__ _FatPointerNoTLB& operator += ( size_t offset )
+	{
+		return move(offset);
+ 	}
+
+	__device__ _FatPointerNoTLB& operator -= ( size_t offset )
+	{
+		return move(-offset);
+	}
+
+public:
+
+	size_t m_fid;	// Should be spilled to local memory
+	off_t m_start;	// Should be spilled to local memory
+	size_t m_end;	// Should be spilled to local memory
+
+	_FatPtr m_ptr;
+
+	uchar* m_mem;
+	PFrame* m_frames;
 };
 
 #endif
