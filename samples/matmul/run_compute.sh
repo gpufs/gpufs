85df0920a223e176729f67eb49898a3df3656231
sagi <sagi@tx.technion.ac.il>
2016-04-12 16:12:52 +0300

    Fixed build issues in matrix multiply

--- samples/matmul/run_compute.sh
+++ samples/matmul/run_compute.sh
@@ -9,8 +9,7 @@ for i in 104 208 416 832 1664 3328; do
 	    for k in 64 128 256;  do
 		echo RUN HA=$i  WA=$j  WB=$k ./mm >> experiment_res
 		for z in 0 1 2; do
-			echo NUM_ITER=1 HA=$i  WA=$j  WB=$k ./mm 2>&1
-# | tee -a all_results |grep RESULT >>experiment_res
+			NUM_ITER=1 HA=$i  WA=$j  WB=$k ./mm 2>&1 | tee -a all_results |grep RESULT >>experiment_res
 		done
 	   done
 	done
